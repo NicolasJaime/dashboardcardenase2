059fac4d004aedd329cf74b3cd31b885309ad740
cadenastjaime <cadenastjaime@gmail.com>
2025-09-24 15:27:53 +0000

    ✨feat(components): Crear el componente IconButton - Crear el componente IconButton.tsx, para su porterior uso em la creación de botones

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,11 @@
-import { Image, Text, View } from "react-native";
+import ProfileDetails from "@/components/ProfileDetails";
+import "@/global.css";
+import { View } from "react-native";
 import "../global.css";
-import "@/global.css"
 export default function Index() {
   return (
-    <View
-    style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <View className="flex justify-center">
-        <Image className="max-h-24 max-w-24 rounded-full" source={require('../assets/images/icon.png')} />
-      </View>
-      <Text className="text-2xl font-bold">
-        Urgiles
-      </Text>
-      <Text className="font-bold text-stone-600">
-        Emilia 
-      </Text>
+    <View className="flex ">
+      <ProfileDetails></ProfileDetails>
       
     </View>
   );
