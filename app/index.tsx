4f7047e74a4b0842891d52878ecb944fc800ba68
cadenastjaime <cadenastjaime@gmail.com>
2025-09-24 15:27:27 +0000

    ✨feat(components): Crear el componente ProfileDetails - Crear la carpeta components - Crear el componente ProfileDetails.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
-import { Text, View } from "react-native";
-import "../global.css"
-import "ProfileDetails"
+import { Image, Text, View } from "react-native";
+import "../global.css";
+import "@/global.css"
 export default function Index() {
   return (
     <View
@@ -11,9 +11,16 @@ export default function Index() {
     }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-        <View className="flex justify-center items-center white w-screen h-screen">
-        <ProfileDetails></ProfileDetails>
-        </View>
+      <View className="flex justify-center">
+        <Image className="max-h-24 max-w-24 rounded-full" source={require('../assets/images/icon.png')} />
+      </View>
+      <Text className="text-2xl font-bold">
+        Urgiles
+      </Text>
+      <Text className="font-bold text-stone-600">
+        Emilia 
+      </Text>
+      
     </View>
   );
 }
